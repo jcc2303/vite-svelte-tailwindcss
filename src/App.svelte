9baf62c845098e26235674bca0f3633a7fd85012
Javier Cabrera <cabrera.javier@gmail.com>
2021-05-01 19:25:50 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+
+  import './Tailwind.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1 class="bg-gray-400 filter blur-md grayscale invert">Hello Typescript!</h1>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+
